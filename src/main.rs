6b121a48191cf6fba38954369a972aedc7fd47a0
jiannei <longjian.huang@foxmail.com>
2024-09-13 09:34:47 +0800

    feat(storage): integrate tracing for fs operations in opendal

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,48 @@
 use anyhow::Result;
+use opendal::layers::TracingLayer;
+use opendal::services::Fs;
+use opendal::Operator;
+use std::path::PathBuf;
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::util::SubscriberInitExt;
+use tracing_subscriber::EnvFilter;
 
-#[tokio::main]
-async fn main() -> Result<()> {
-    println!("Hello,world");
+fn main() -> Result<()> {
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
+    let runtime = tokio::runtime::Runtime::new()?;
+
+    runtime.block_on(test_fs())?;
+
+    Ok(())
+}
+
+async fn test_fs() -> Result<()> {
+    let base_path = std::env::var("CARGO_MANIFEST_DIR").map(PathBuf::from)?;
+
+    // Create fs backend builder.
+    let mut builder = Fs::default()
+        // Set the root for fs, all operations will happen under this root.
+        //
+        // NOTE: the root must be absolute path.
+        .root(&base_path.join("storage").as_path().to_string_lossy());
+
+    // `Accessor` provides the low level APIs, we will use `Operator` normally.
+    let op: Operator = Operator::new(builder)?.layer(TracingLayer).finish();
+
+    op.write("hello.log", "Hello OpenDAL!").await?;
+
+    let content = op.read("hello.log").await?.current();
+
+    dbg!(content);
+
+    let stat = op.stat("hello.log").await?;
+
+    op.copy("hello.log", "hello2.log").await?;
+
+    dbg!(stat);
 
     Ok(())
 }
