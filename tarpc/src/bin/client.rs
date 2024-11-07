69c20f37af6ac2c39d490ac39d24c6c868d3d7fe
jiannei <longjian.huang@foxmail.com>
2024-11-08 01:33:41 +0800

    refactor(client): improve code readability and maintainability

--- tarpc/src/bin/client.rs
+++ tarpc/src/bin/client.rs
@@ -1,9 +1,11 @@
 use service::HelloClient;
+use std::net::{IpAddr, Ipv6Addr};
 use tarpc::{client, context, tokio_serde::formats::Json};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let mut transport = tarpc::serde_transport::tcp::connect("[::1]:50051", Json::default);
+    let server_addr = (IpAddr::V6(Ipv6Addr::LOCALHOST), 50051);
+    let mut transport = tarpc::serde_transport::tcp::connect(&server_addr, Json::default);
     transport.config_mut().max_frame_length(usize::MAX);
 
     let client = HelloClient::new(client::Config::default(), transport.await?).spawn();
