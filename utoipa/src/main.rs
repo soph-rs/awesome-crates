43478b3da1fb639fad9f31e5978eb6680d7c4bd9
jiannei <longjian.huang@foxmail.com>
2024-12-16 11:17:49 +0800

    feat(utoipa): implement todo API example with OpenAPI documentation

--- utoipa/src/main.rs
+++ utoipa/src/main.rs
@@ -1,32 +1,60 @@
-use std::net::SocketAddr;
+mod todo;
 
-use axum::{routing::get, Json};
-use utoipa::OpenApi;
+use std::net::{Ipv4Addr, SocketAddr};
+
+use std::io::Error;
+use tokio::net::TcpListener;
+use utoipa::{
+    openapi::security::{ApiKey, ApiKeyValue, SecurityScheme},
+    Modify, OpenApi,
+};
+use utoipa_axum::router::OpenApiRouter;
+use utoipa_rapidoc::RapiDoc;
+use utoipa_redoc::{Redoc, Servable};
+use utoipa_scalar::{Scalar, Servable as ScalarServable};
+use utoipa_swagger_ui::SwaggerUi;
+
+const TODO_TAG: &str = "todo";
 
 #[derive(OpenApi)]
-#[openapi(paths(openapi))]
-struct ApiDoc;
-
-/// Return JSON version of an OpenAPI schema
-#[utoipa::path(
-    get,
-    path = "/api-docs/openapi.json",
-    responses(
-        (status = 200, description = "JSON file", body = ())
+#[openapi(
+    modifiers(&SecurityAddon),
+    tags(
+            (name = TODO_TAG, description = "Todo items management API")
     )
 )]
-async fn openapi() -> Json<utoipa::openapi::OpenApi> {
-    Json(ApiDoc::openapi())
+struct ApiDoc;
+
+struct SecurityAddon;
+
+impl Modify for SecurityAddon {
+    fn modify(&self, openapi: &mut utoipa::openapi::OpenApi) {
+        if let Some(components) = openapi.components.as_mut() {
+            components.add_security_scheme(
+                "api_key",
+                SecurityScheme::ApiKey(ApiKey::Header(ApiKeyValue::new("todo_apikey"))),
+            )
+        }
+    }
 }
 
 #[tokio::main]
-async fn main() {
-    let socket_address: SocketAddr = "127.0.0.1:8000".parse().unwrap();
-    let listener = tokio::net::TcpListener::bind(socket_address).await.unwrap();
+async fn main() -> Result<(), Error> {
+    let (router, api) = OpenApiRouter::with_openapi(ApiDoc::openapi())
+        .nest("/api/v1/todos", todo::router())
+        .split_for_parts();
 
-    let app = axum::Router::new().route("/api-docs/openapi.json", get(openapi));
+    let router = router
+        .merge(SwaggerUi::new("/swagger-ui").url("/api-docs/openapi.json", api.clone()))
+        .merge(Redoc::with_url("/redoc", api.clone()))
+        // There is no need to create `RapiDoc::with_openapi` because the OpenApi is served
+        // via SwaggerUi instead we only make rapidoc to point to the existing doc.
+        .merge(RapiDoc::new("/api-docs/openapi.json").path("/rapidoc"))
+        // Alternative to above
+        // .merge(RapiDoc::with_openapi("/api-docs/openapi2.json", api.clone()).path("/rapidoc"))
+        .merge(Scalar::with_url("/scalar", api));
 
-    axum::serve(listener, app.into_make_service())
-        .await
-        .unwrap()
+    let address = SocketAddr::from((Ipv4Addr::UNSPECIFIED, 8000));
+    let listener = TcpListener::bind(&address).await?;
+    axum::serve(listener, router.into_make_service()).await
 }
