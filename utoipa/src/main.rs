0efcec40f3c8fd3cb1db77b24f5a4b4d3264fb88
jiannei <longjian.huang@foxmail.com>
2024-12-16 11:02:27 +0800

    feat(awesome-crates): add utoipa demo and update dependencies

---
+++ utoipa/src/main.rs
@@ -0,0 +1,32 @@
+use std::net::SocketAddr;
+
+use axum::{routing::get, Json};
+use utoipa::OpenApi;
+
+#[derive(OpenApi)]
+#[openapi(paths(openapi))]
+struct ApiDoc;
+
+/// Return JSON version of an OpenAPI schema
+#[utoipa::path(
+    get,
+    path = "/api-docs/openapi.json",
+    responses(
+        (status = 200, description = "JSON file", body = ())
+    )
+)]
+async fn openapi() -> Json<utoipa::openapi::OpenApi> {
+    Json(ApiDoc::openapi())
+}
+
+#[tokio::main]
+async fn main() {
+    let socket_address: SocketAddr = "127.0.0.1:8000".parse().unwrap();
+    let listener = tokio::net::TcpListener::bind(socket_address).await.unwrap();
+
+    let app = axum::Router::new().route("/api-docs/openapi.json", get(openapi));
+
+    axum::serve(listener, app.into_make_service())
+        .await
+        .unwrap()
+}
